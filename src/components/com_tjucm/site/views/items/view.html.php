691cb5fe8863b872e019b8d4c0c38be80df1630e
“Sandip <sandip_p@tekditechnologies.com>
2018-10-06 16:04:30 +0530

    Task #787 chor: change year

--- src/components/com_tjucm/site/views/items/view.html.php
+++ src/components/com_tjucm/site/views/items/view.html.php
@@ -3,7 +3,7 @@
  * @version    SVN: <svn_id>
  * @package    Com_Tjucm
  * @author     Techjoomla <extensions@techjoomla.com>
- * @copyright  Copyright (c) 2009-2017 TechJoomla. All rights reserved.
+ * @copyright  Copyright (c) 2009-2018 TechJoomla. All rights reserved.
  * @license    GNU General Public License version 2 or later.
  */
 
